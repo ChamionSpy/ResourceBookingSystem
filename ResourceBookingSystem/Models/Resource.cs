0afff1073667b4a65ec32d2e481604a465c5de26
LihleM <LihleM@LIHLEM-LT01>
2025-07-15 06:43:55 +0200

    Added and updated resource model

---
+++ ResourceBookingSystem/Models/Resource.cs
@@ -0,0 +1,12 @@
+﻿namespace ResourceBookingSystem.Models
+{
+    public class Resource
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Location { get; set; }
+        public int Capacity { get; set; }
+        public bool IsAvailable { get; set; }
+    }
+}
