b7b4a1e457f472dd975ed08d8c029708a2f37d8d
LihleM <LihleM@LIHLEM-LT01>
2025-07-15 07:01:31 +0200

    Added a resource controller using MVC Controller with views using Entity Framework. This hangles CRUD logic in the controller and greates the views for the resources. I also added server-side validation for Name and Capacity.

--- ResourceBookingSystem/Models/Resource.cs
+++ ResourceBookingSystem/Models/Resource.cs
@@ -1,11 +1,17 @@
-﻿namespace ResourceBookingSystem.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ResourceBookingSystem.Models
 {
     public class Resource
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
         public string Name { get; set; }
         public string Description { get; set; }
         public string Location { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Capacity must be greater than 0")]
         public int Capacity { get; set; }
         public bool IsAvailable { get; set; }
     }
