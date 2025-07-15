eec2d23d37fe29f27103eee97b14ba15a52b5327
LihleM <LihleM@LIHLEM-LT01>
2025-07-15 06:52:03 +0200

    Added ApplicationDbContext class that inherits from DbContext and added DbSet for resource model. I also registered DbContext in program.cs

---
+++ ResourceBookingSystem/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ResourceBookingSystem.Models;
+
+namespace ResourceBookingSystem.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Resource> Resource { get; set; }
+    }
+}
