f14e083d871c41c830b870cd59aa89db4a2959fc
LihleM <LihleM@LIHLEM-LT01>
2025-07-15 07:34:55 +0200

    Deleted the initial database and recreated it to include the bookings table.

--- ResourceBookingSystem/Models/Booking.cs
+++ ResourceBookingSystem/Models/Booking.cs
@@ -1,16 +1,17 @@
-﻿namespace ResourceBookingSystem.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ResourceBookingSystem.Models
 {
     public class Booking
     {
         public int Id { get; set; }
+
+        // This points to the resource beign booked
         public int ResourceId { get; set; }
         public DateTime StartTime { get; set; }
         public DateTime EndTime { get; set; }
         public string BookedBy { get; set; }
         public string Purpose { get; set; }
-
-        // This points to the resource beign booked
-        public int ResourceID { get; set; }
 
         // This lets us get the full resource details from a booking
         public Resource Resource { get; set; }
